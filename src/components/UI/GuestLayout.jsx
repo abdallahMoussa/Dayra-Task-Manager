5101482563c6d83c69deef2f9365f9c42bad6bb2
abdallah.moussa2017@outlook.com <abdallah.moussa2017@outlook.com>
2023-11-28 00:07:18 +0200

    commit

--- src/components/UI/GuestLayout.jsx
+++ src/components/UI/GuestLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ThemeContext from "../../context/ThemeContext";
 import DarkmodeButton from "./DarkmodeButton";
 import LangButton from "./LangButton";
@@ -8,15 +8,26 @@ const GuestLayout = () => {
   const { isArabic } = useContext(ThemeContext);
   const { auth } = useContext(AuthContext);
 
-  return auth ? null : (
-    <div
-      className={`fixed flex flex-col items-center z-50 bottom-2 ${
-        isArabic ? "left-2" : "right-2"
-      }`}
-    >
-      <DarkmodeButton className="w-[36px] scale-[1.1] overflow-hidden" />
-      <LangButton className="scale-125 mt-1 " />
-    </div>
+  const [rendered, setRendered] = useState(false);
+
+  useEffect(() => {
+    if (!auth) {
+      setRendered(true);
+    }
+  }, []);
+  return (
+    <>
+      {rendered ? (
+        <div
+          className={`fixed flex flex-col items-center z-50 bottom-2 ${
+            isArabic ? "left-2" : "right-2"
+          }`}
+        >
+          <DarkmodeButton className="w-[36px] scale-[1.1] overflow-hidden" />
+          <LangButton className="scale-125 mt-1 " />
+        </div>
+      ) : null}
+    </>
   );
 };
 
